b5b89b9c4f46408b7d70cd3c63f687b1ddacc093
alexandramorrison <morrison.alexx@gmail.com>
2020-10-15 15:32:33 +0300

    edited map section, added API

--- js/script.js
+++ js/script.js
@@ -19,3 +19,26 @@ window.addEventListener('keydown', function (evt) {
     popup.classList.remove('modal-show');
   }
 });
+
+
+/* MAP */
+
+
+function initMap() {
+  let coordinates = {lat: 59.938655, lng: 30.323206};
+  let markerImage = 'img/map-marker.png';
+  let centerMap = {lat: 59.939107, lng: 30.321275};
+  let map = new google.maps.Map(
+      document.getElementById('map'), {
+        zoom: 17,
+        center: centerMap,
+        disableDefaultUI: true,
+        scrollwheel: false
+      });
+
+      let marker = new google.maps.Marker({
+        position: coordinates,
+        map: map,
+        icon: markerImage
+      });
+};
