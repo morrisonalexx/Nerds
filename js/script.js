29164b05c3b9530dd417bddb0a5d7e1170277d83
alexandramorrison <morrison.alexx@gmail.com>
2020-10-20 00:40:26 +0300

    edited criterias, added minified version of script.js and style.css

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
-let popup = document.querySelector('.modal');
-let openPopupButton = document.querySelector('.write-us');
-let closePopupButton = popup.querySelector('.modal-close');
+let popup = document.querySelector(".modal");
+let openPopupButton = document.querySelector(".write-us");
+let closePopupButton = popup.querySelector(".modal-close");
 let userName = popup.querySelector(".user-name");
 let feedbackForm = popup.querySelector(".feedback-form");
 let userEmail= popup.querySelector(".user-email");
 
-openPopupButton.addEventListener('click', function (evt) {
+openPopupButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add('modal-show');
+  popup.classList.add("modal-show");
+  userName.focus({preventScroll:true});
 });
 
-closePopupButton.addEventListener('click', function (evt) {
-  popup.classList.remove('modal-show');
+closePopupButton.addEventListener("click", function (evt) {
+  popup.classList.remove("modal-show");
 });
 
-window.addEventListener('keydown', function (evt) {
+window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    popup.classList.remove('modal-show');
+    popup.classList.remove("modal-show");
   }
 });
 
@@ -26,10 +27,10 @@ window.addEventListener('keydown', function (evt) {
 
 function initMap() {
   let coordinates = {lat: 59.938655, lng: 30.323206};
-  let markerImage = 'img/map-marker.png';
+  let markerImage = "img/map-marker.png";
   let centerMap = {lat: 59.939107, lng: 30.321275};
   let map = new google.maps.Map(
-      document.getElementById('map'), {
+      document.getElementById("map"), {
         zoom: 17,
         center: centerMap,
         disableDefaultUI: true,
